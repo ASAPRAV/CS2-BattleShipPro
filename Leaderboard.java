83a3cfbb38aeb2af21e48ec99a12d79e06e06482
alexgoodell1 <ajgoody12@gmail.com>
2015-05-20 12:40:13 -0700

    Wrote constructor

--- Leaderboard.java
+++ Leaderboard.java
@@ -1,4 +1,24 @@
+import java.io.BufferedWriter;
+import
 public class Leaderboard{
+  private File f;
+  private FileWriter fw;
+  private BufferedWriter bw;
 
-  
+public Leaderboard{
+
+  try{
+    f = new File("./leaderboard.txt");
+    if(!f.exists()){
+      fw = new FileWrtier(f.getAbsoluteFile());
+      bw = new BufferedWriter(fw);
+      bw.write("Leaderboard:");
+    }
+    catch(Exception e){
+      e.printStackTrace();
+    }
+  }
+}
+
+
 }
