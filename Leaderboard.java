ed708d352080af553f5834327f75ead49b4c68e8
alexgoodell1 <ajgoody12@gmail.com>
2015-05-27 09:16:43 -0700

    Wrote writeScore and readLeaderboard method

--- Leaderboard.java
+++ Leaderboard.java
@@ -1,12 +1,14 @@
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
-import java.util.Scanner;
+import java.util.Scanner
+import java.util.ArrayList;
 
 public class Leaderboard{
   private File f;
   private FileWriter fw;
   private BufferedWriter bw;
+  private Scanner in;
 
 public Leaderboard(){
 
@@ -17,10 +19,40 @@ public Leaderboard(){
       fw = new FileWriter(f.getAbsoluteFile());
       bw = new BufferedWriter(fw);
       bw.write("Leaderboard:");
+      bw.close();
     }
   }
     catch(Exception e){
       e.printStackTrace();
     }
   }
+  public ArrayList<String> readLeaderboard(){
+    ArrayList<String> scores = new ArrayList<String>();
+    try{
+      in = new Scanner(f);
+      while(in.hasNext()){
+        scores.add(in.nextLine());
+      }
+    }catch(Exception e){
+      e.printStackTrace();
+    }
+    return scores;
+  }
+
+
+public void writeScores(String Score){
+    try{
+      f = new File("./leaderboard.txt");
+      if(!f.exists()){
+        System.out.println("File doesnt exist, create first");
+      }
+      else{
+        fw = new FileWriter(f.getAbsoluteFile(),true);
+        bw = new BufferedWriter(fw);
+        bw.write(Score);
+        bw.close();
+
+      }
+    }
+}
 }
