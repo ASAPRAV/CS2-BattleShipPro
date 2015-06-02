1a6fe0867759bcb4ea7d92f39278694110e96b28
mr-swag <jpennington565@go.tahomasd.us>
2015-06-02 03:40:44 -0700

    graphics for start screen + setup screens
    window would not resize to display the whole thing
    
    Still needs:
    mouselistener for setup screens + player screens
    -should be easy enough if statements

--- game.java
+++ game.java
@@ -10,6 +10,7 @@ public class game extends Applet implements MouseListener
    private Graphics backg;
    private static player player1;
    private static player player2;
+   private static String screen;
    
    public void init()
    {
@@ -18,7 +19,7 @@ public class game extends Applet implements MouseListener
       Color p1 = Color.red;
       player2 = new player();
       Color p2 = Color.blue;
-            
+      screen = "start";      
       //adds mouse listener
       addMouseListener(this);
       
@@ -30,13 +31,68 @@ public class game extends Applet implements MouseListener
    public void update(Graphics g)
    {
       //draws the grid
-      drawSite();
+      if(screen.equals("start"))
+         startScreen();
+      else if(screen.equals("player1setup"))
+         player1SetupScreen();
+      else if(screen.equals("player2setup"))
+         player2SetupScreen();
+      else if(screen.equals("player1"))
+         player1Screen();
+      else if(screen.equals("player2"))
+         player2Screen();
       g.drawImage(backbuffer, 0,0, this); //only g
    }
    
    public void paint(Graphics g)
-   { update(g); }
+   { 
+      this.setSize(300, 600);
+      update(g); 
+   }
    
+   public void startScreen()
+   {
+      backg.setColor(Color.lightGray);
+      backg.fillRect(0,0,300, 600);
+      backg.setColor(Color.blue);
+      backg.fillRoundRect(100, 275, 100, 50, 10, 5);
+      backg.setColor(Color.black);
+      backg.drawString("START",130, 305);
+   }
+   public void player1SetupScreen()
+   {
+      backg.setColor(Color.darkGray);
+      backg.fillRect(0,0,300,25);
+      backg.fillRect(0,275,300,50);
+      backg.fillRect(0,575,300,25);
+      backg.fillRect(0,0,25,600);
+      backg.fillRect(275,0,25,600);
+      backg.setColor(Color.black);
+      for(int r = 0; r < 9; r++)
+      {
+         backg.drawLine(50 + 25*r, 25, 50 + 25*r, 275);
+         backg.drawLine(50 + 25*r, 325, 50 + 25*r, 575);
+      }
+      for(int c =0; c <9; c++)
+      {
+         backg.drawLine(25, 50 + 25*c, 275, 50 + 25*c);
+         backg.drawLine(25, 350 + 25*c, 275, 350 + 25*c);
+      }
+   }
+   public void player2SetupScreen()
+   {
+   
+   }
+   public void player1Screen()
+   {
+   
+   }
+   public void player2Screen()
+   {
+   
+   }
+   
+   /*
    public void drawSite()
    {
       //draws the grid
@@ -89,13 +145,37 @@ public class game extends Applet implements MouseListener
          }
       }
    }
-   
+   */
    public void mouseClicked(MouseEvent e)
    {
       //get mouse coordinates
       int x = e.getX();
       int y = e.getY();
       //make sure its within the bounds
+      
+      if(screen.equals("start"))
+      {
+         if(x >= 100 && x <= 200 && y >=275 && y< 325)
+         screen = "player1setup";
+         repaint();
+      }
+      else if(screen.equals("player1Setup"))
+      {
+         
+      }
+      else if(screen.equals("player2Setup"))
+      {
+      
+      }
+      else if(screen.equals("player1"))
+      {
+      
+      }
+      else if(screen.equals("player2"))
+      {
+      
+      }
+      /*
       if(x>=50 && x<= 550 && y>=50 && y<=550)
       {
          //modify it
@@ -107,6 +187,7 @@ public class game extends Applet implements MouseListener
             dig(xBox, yBox);//call dig upon that spot
          }
       }
+      */
    }
    
    public void mouseEntered(MouseEvent e)
