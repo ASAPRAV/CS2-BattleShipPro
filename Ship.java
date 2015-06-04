d72f5af26c613b12e8bee593da78c776ad5de9bc
mr-swag <student@406-imac16.tahomasd.private>
2015-06-04 08:59:29 -0700

    Ships & Spots are merged from master

--- Ship.java
+++ Ship.java
@@ -1,11 +1,64 @@
 public class Ship
 {
+<<<<<<< HEAD
    private int size;
    private boolean sunk;
    public int[][] locs;
    public Ship(int len)
+=======
+   public static int size;
+   public static spot[][] ships;
+   private static boolean sunk;
+   private static int startX;
+   private static int startY;
+   private static int endX;
+   private static int endY;
+   
+   public Ship(int len, int startXe, int startYe, int endXe, int endYe)
+>>>>>>> master
    {
+      startX = startXe;
+      startY = startYe;
+      endX = endXe;
+      endY = endYe;
+      
       sunk = false;
+<<<<<<< HEAD
       locs = new int[len][2];
+=======
+      ships = new spot[10][10];
+      for(int r = 0; r < 10; r++)
+      {
+         for(int c = 0; c < 10; c++)
+         {
+            ships[r][c] = new spot();
+         }
+      }
+      
+      size = len;
+      
+      for(int r = startX; r < endX; r++)
+      {
+         for(int c = startY; c < endY; c++)
+         {
+            ships[r][c].makeShip();
+         }
+      } 
+   }
+      
+   public boolean isSunk()
+   {
+      for(int r = startX; r < endX; r++)
+      {
+         for(int c = startY; c <endY; c++)
+         {
+            if(ships[r][c].isShip() && !ships[r][c].getGuess())
+            {
+               return false;
+            }
+         }
+      }
+      return true;
+>>>>>>> master
    }
 }
